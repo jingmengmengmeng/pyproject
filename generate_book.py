2c7f40424af886fc947dca26632b25d96f587580
jingmengmengmeng <609354486@qq.com>
2019-04-14 16:04:44 +0800

    update

--- generate_book.py
+++ generate_book.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import os
+import pymysql
 
 
 def get_book(n):
@@ -15,6 +16,29 @@ def get_book(n):
         book_map = {"bookname": book_name, "author_name": author_name}
         book_list.append(book_map)
     return book_list
+
+
+def write_mysql(contents):
+    db = get_mysql_connection()
+    for i in range(0, len(contents)):
+        insert(db, "books", contents[i]["bookname"],contents[i]["author_name"])
+
+    db.close()
+
+
+def get_mysql_connection():
+    # 打开数据库连接
+    db = pymysql.connect("100.73.141.147", "root", "root", "test")
+
+    return db
+
+
+def insert(db, table_name, bookname, author_name):
+    cursor = db.cursor()
+    sql = "insert into " + table_name + "(book_name,author_name) " + "values(\"" + bookname + "\",\"" + author_name + "\")"
+    print(sql)
+    cursor.execute(sql)
+    db.commit()
 
 
 def write_files(book_list):
@@ -39,5 +63,5 @@ def write_file(file_name, content):
 
 if __name__ == "__main__":
     book_list = get_book(4)
-    write_files(book_list)
+    write_mysql(book_list)
     print(book_list)
